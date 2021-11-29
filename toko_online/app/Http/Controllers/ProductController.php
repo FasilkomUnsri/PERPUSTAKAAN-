bf4105cced81d29c639b5372f761c13576ed3e81
nabilaputrim <nabila.peem@gmail.com>
2021-11-29 10:07:47 +0700

    tambah database , edit , dan delete

--- toko_online/app/Http/Controllers/ProductController.php
+++ toko_online/app/Http/Controllers/ProductController.php
@@ -11,4 +11,36 @@ class productcontroller extends Controller
        $products = product::all();
         return view('products.index',compact(['products']));
     }
+
+    public function create()
+    {
+        return view('products.create');
+    }
+
+    public function store(Request $request)
+    {
+        Product::create($request->all());
+        return redirect('/products');
+    }
+
+    public function edit($id)
+    {
+        $product = Product::find($id);
+        return view('products.edit',compact(['product']));
+    }
+
+    public function update(Request $request,$id)
+    {
+        $product = Product::find($id);
+        $product->update($request->all());
+        return redirect('/products');
+    }
+
+    public function destroy($id)
+    {
+        $product = Product::find($id);
+        $product->delete();
+        return redirect('/products');
+    }
+
 }
--- toko_online/routes/web.php
+++ toko_online/routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use App\Models\Product;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,3 +16,8 @@ use App\Http\Controllers\ProductController;
 */
 
 Route::get('/products',[ProductController::class,'index'] );
+Route::get('/products/create',[ProductController::class,'create'] );
+Route::post('/products',[ProductController::class,'store'] );
+Route::get('/products/{id}/edit',[ProductController::class,'edit'] );
+Route::put('/products/{id}',[ProductController::class,'update']);
+Route::delete('/products/{id}',[ProductController::class,'destroy']);
