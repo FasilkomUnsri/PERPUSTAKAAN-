1d944466c34b4ea01f5544f6bbd6256d8266f593
teddy792 <90497381+teddy792@users.noreply.github.com>
2021-11-30 14:55:45 +0700

    mengubah judul menjadi perpustakaan menambahkan database  auth login and register dan menambahkan template

---
+++ perpustakaan/database/migrations/2021_11_30_073629_create_buku_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBukuTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('buku', function (Blueprint $table) {
+            $table->id();
+            $table->string('judul');
+            $table->string('slug');
+            $table->string('sampul');
+            $table->string('penulis');
+            $table->foreignid('penerbit_id');
+            $table->foreignid('kategori_id');
+            $table->foreignid('rak_id');
+            $table->integer('stok');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('buku');
+    }
+}
